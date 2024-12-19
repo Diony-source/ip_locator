c0ba05bfd0263b85a3dc65089fadba4bc27321bd
Diony-source <erendalkiran2001@gmail.com>
2024-12-19 20:33:26 +0300

    feat(entities): Defined struct for IP location data.

--- entities/entities.go
+++ entities/entities.go
@@ -3,12 +3,13 @@ package entities
 
 // IPLocation represents the response structure from the API
 type IPLocation struct {
-	IP          string `json:"ip"`
-	Country     string `json:"country_name"`
-	Region      string `json:"region_name"`
-	City        string `json:"city"`
-	Latitude    string `json:"latitude"`
-	Longitude   string `json:"longitude"`
-	ISP         string `json:"isp"`
+	IP          string  `json:"query"`
+	Country     string  `json:"country"`
+	Region      string  `json:"regionName"`
+	City        string  `json:"city"`
+	Latitude    float64 `json:"lat"`
+	Longitude   float64 `json:"lon"`
+	ISP         string  `json:"isp"`
 	Organization string `json:"org"`
+	Status      string  `json:"status"`
 }
